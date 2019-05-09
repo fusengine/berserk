a70ebeeb74f9a8c27f375cfff7d9248f0c3b7275
Bruno Azoulay <fusengine@users.noreply.github.com>
2019-05-09 10:24:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,token')
-	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
 	next()
 })
 
